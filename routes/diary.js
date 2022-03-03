4dc23d92e42b2a907c780c92d267861a6f9819ff
Sangmin Na <sangmin.iam@gmail.com>
2022-03-03 22:17:54 +0900

    feat: get-paginated-diaries & non-paginated-dairies

--- routes/diary.js
+++ routes/diary.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { createDiary } = require("../controllers/diary.controller.js");
+const {
+  createDiary,
+  getPaginatedDiaries,
+  getDiaries,
+} = require("../controllers/diary.controller.js");
 const { uploadS3 } = require("../middlewares/multer.js");
 const { verifyToken } = require("../middlewares/verifyToken");
 
+router.get("/", verifyToken, getDiaries);
+router.get("/pagination", verifyToken, getPaginatedDiaries);
 router.post("/", verifyToken, uploadS3.single("audio"), createDiary);
 
 module.exports = router;
