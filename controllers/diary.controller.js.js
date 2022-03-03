2d5dde64d8adef302238a8872d4aa01e83dafe45
Sangmin Na <sangmin.iam@gmail.com>
2022-03-03 22:17:54 +0900

    feat: get-paginated-diaries & non-paginated-dairies

--- controllers/diary.controller.js.js
+++ controllers/diary.controller.js.js
@@ -1,7 +1,9 @@
+const createError = require("http-errors");
+const differenceInMonths = require("date-fns/differenceInMonths");
+const Sentiment = require("sentiment");
+
 const Diary = require("../models/Diary");
 const { User } = require("../models/User");
-const Sentiment = require("sentiment");
-const createError = require("http-errors");
 
 const sentiment = new Sentiment();
 
@@ -28,6 +30,73 @@ exports.createDiary = async (req, res, next) => {
     res.json({
       result: "success",
       data: null,
+    });
+  } catch (err) {
+    next(createError(err));
+  }
+};
+
+exports.getPaginatedDiaries = async (req, res, next) => {
+  try {
+    let query = Diary.find();
+
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.limit) || 9;
+    const skip = (page - 1) * pageSize;
+    const total = await Diary.countDocuments();
+
+    const pages = Math.ceil(total / pageSize);
+
+    query = query.skip(skip).limit(pageSize);
+
+    if (page > pages) {
+      next();
+      return;
+    }
+
+    const result = await query;
+
+    res.status(200).json({
+      result: "success",
+      data: {
+        page,
+        pages,
+        diaries: result,
+        count: result.length,
+      },
+    });
+  } catch (err) {
+    next(createError(err));
+  }
+};
+
+exports.getDiaries = async (req, res, next) => {
+  try {
+    const { startDate, endDate } = req.query;
+
+    const monthGap = differenceInMonths(new Date(endDate), new Date(startDate));
+
+    if (startDate > endDate || monthGap > 12) {
+      next(createError.BadRequest());
+      return;
+    }
+
+    const { email } = req.user;
+    const { _id: userId } = await User.findOne({ email }).select("_id").lean();
+
+    const diaries = await Diary.find({
+      createdBy: userId,
+      $and: [
+        { createdAt: { $gte: startDate } },
+        { createdAt: { $lte: endDate } },
+      ],
+    });
+
+    res.json({
+      result: "success",
+      data: {
+        diaries,
+      },
     });
   } catch (err) {
     next(createError(err));
