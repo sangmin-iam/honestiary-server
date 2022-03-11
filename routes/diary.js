b08efb22d0a1341f0082367b53aec3d701da9b9a
Sangmin Na <sangmin.iam@gmail.com>
2022-03-12 02:44:28 +0900

    feat: delete diary

--- routes/diary.js
+++ routes/diary.js
@@ -5,13 +5,15 @@ const {
   createDiary,
   getDiaries,
   getDiary,
+  deleteDiary,
 } = require("../controllers/diary.controller.js");
-const { uploadS3 } = require("../middlewares/multer.js");
+const { uploadS3, deleteDiaryS3 } = require("../middlewares/multer.js");
 const { verifyToken } = require("../middlewares/verifyToken");
 
 router.get("/", verifyToken, getDiaries);
 router.post("/", verifyToken, uploadS3.single("audio"), createDiary);
 
 router.get("/:diary_id", verifyToken, getDiary);
+router.delete("/:diary_id", verifyToken, deleteDiaryS3, deleteDiary);
 
 module.exports = router;
